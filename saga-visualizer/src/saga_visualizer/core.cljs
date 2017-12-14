31ab9c4ad56548683d0a723d1c5a9a066e1540d2
seanyinx <sean.yin@huawei.com>
2017-12-14 11:52:47 +0800

    JAV-502 replaced div graph with svg for easier drawing
    Signed-off-by: seanyinx <sean.yin@huawei.com>

--- saga-visualizer/src/saga_visualizer/core.cljs
+++ saga-visualizer/src/saga_visualizer/core.cljs
@@ -8,7 +8,7 @@
 (defonce current-request (reagent/atom (reagent/atom nil)))
 (defonce dragging-request (reagent/atom nil))
 (defonce lines (reagent/atom {}))
-(defonce node-positions (reagent/atom {}))
+(defonce connector-positions (reagent/atom {}))
 
 (defn json-ui []
   [:div {:class "modal-content"}
@@ -22,63 +22,37 @@
 (defonce request-count (reagent/atom 0))
 
 (defn request-icon []
-  [:div {
-         :on-click #(swap! request-count inc)
-         :style    {:width            30
-                    :height           20
-                    :top              100
-                    :left             50
-                    :border-style     "solid"
-                    :border-color     "black"
-                    :background-color "white"}}])
+  [:rect {
+          :on-click #(swap! request-count inc)
+          :width    30
+          :height   20
+          :y        100
+          :x        50
+          :stroke   "black"
+          :fill     "gray"}])
+
+(defn global-coordinate [elem]
+  (let [svg (.getElementById js/document "request-graph-svg")
+        point (.createSVGPoint svg)]
+    (set! (.-x point) (.-baseVal.value (.-cx elem)))
+    (set! (.-y point) (.-baseVal.value (.-cy elem)))
+    (.matrixTransform point (.getCTM elem))))
 
 (defn position [elem]
-  (let [parent-pos (@node-positions (.-id (.-parentElement elem)))
-        parent-x (:x parent-pos)
-        parent-y (:y parent-pos)
-        parent-width (.-offsetWidth (.-parentElement elem))
-        parent-height (.-offsetHeight (.-parentElement elem))
-        x (.-offsetLeft elem)
-        y (.-offsetTop elem)
-        width (.-offsetWidth elem)
-        height (.-offsetHeight elem)]
-    {:x (cond
-          (< x 0) parent-x
-          (> (+ x width) parent-width) (+ parent-x parent-width)
-          :else (+ parent-x (/ parent-width 2)))
-     :y (cond
-          (< y 0) parent-y
-          (> (+ y height) parent-height) (+ parent-y parent-height)
-          :else (+ parent-y (/ parent-height 2)))})
-  )
-
-
-(defn square [x]
-  (* x x))
-
-(defn degrees [radians]
-  (/ (* radians 180) Math/PI))
+  (let [coordinate (global-coordinate elem)]
+    {:x (.-x coordinate)
+     :y (.-y coordinate)}))
 
 (defn line [start end]
-  (let [start-pos (position (.getElementById js/document start))
-        end-pos (position (.getElementById js/document end))
-        length (Math/sqrt
-                 (+ (square (- (:y end-pos) (:y start-pos)))
-                    (square (- (:x end-pos) (:x start-pos)))))
-        rad (Math/atan
-              (/ (- (:x start-pos) (:x end-pos))
-                 (- (:y end-pos) (:y start-pos))))
-        deg (degrees rad)]
-    [:div {:style (into {:width            1
-                         :height           length
-                         :left             (/ (+ (:x start-pos) (:x end-pos)) 2)
-                         :top              (/ (- (+ (:y start-pos) (:y end-pos)) length) 2)
-                         :position         "absolute"
-                         :background-color "black"}
-                        [{:transform (str "rotate(" deg "deg)")}
-                         {:-webkit-transform (str "rotate(" deg "deg)")}
-                         {:-ms-transform (str "rotate(" deg "deg)")}]
-                        )}]))
+  (let [svg (.getElementById js/document "request-graph-svg")
+        start-pos (@connector-positions start)
+        end-pos (@connector-positions end)]
+    [:polyline {:points       (str (:x start-pos) "," (:y start-pos) " " (:x end-pos) "," (:y end-pos))
+                :fill         "none"
+                :stroke-width "2"
+                :stroke       "black"
+                :marker-end   "url(#Triangle)"}
+     ]))
 
 (defn request-field [field]
   [:div {:class "col-sm-9"}
@@ -108,95 +82,94 @@
     [request-row "Path" :path]]])
 
 
-(defn connector [request pos visible?]
-  (reagent/with-let [border-color (reagent/atom "black")
-                     id (.toString (random-uuid))]
-                    [:div {:id             id
-                           :on-mouse-over  #(reset! border-color "green")
-                           :on-mouse-leave #(reset! border-color "black")
-                           :on-mouse-down  (fn [evt]
-                                             (reset! dragging-request {:connector-id id
-                                                                       :request      @request})
-                                             )
-                           :on-mouse-up    (fn [evt]
-                                             (when (not (nil? dragging-request))
-                                               (swap! request assoc :parents (set (filter #(not= % (:id @request))
-                                                                                          (conj (:parents @request) (:id (:request @dragging-request))))))
-                                               (swap! request-json assoc (:uuid @request) @request)
-                                               (swap! lines assoc (str (:id (:request @dragging-request)) "-" (:id @request))
-                                                      {:start-pos (:connector-id @dragging-request)
-                                                       :end-pos   id})
-                                               (reset! dragging-request nil)))
-                           :style          {:left             (:x pos)
-                                            :top              (:y pos)
-                                            :width            10
-                                            :height           10
-                                            :border-style     "solid"
-                                            :border-color     @border-color
-                                            :position         "absolute"
-                                            :visibility       (if visible?
-                                                                "visible"
-                                                                "hidden")
-                                            :background-color "white"}}]))
+(defn elem-by-id [id]
+  (let [svg (.getElementById js/document "request-graph-svg")]
+    (.getElementById svg id)))
+
+(defn connector [id request pos visible?]
+  (reagent/with-let [border-color (reagent/atom "black")]
+                    [:circle {:id             id
+                              :on-mouse-over  #(reset! border-color "green")
+                              :on-mouse-leave #(reset! border-color "black")
+                              :on-mouse-down  (fn [evt]
+                                                (reset! dragging-request {:connector-id id
+                                                                          :request      @request})
+                                                (swap! connector-positions assoc id (position (elem-by-id id))))
+                              :on-mouse-up    (fn [evt]
+                                                (when (not (nil? dragging-request))
+                                                  (swap! request assoc :parents (set (filter #(not= % (:id @request))
+                                                                                             (conj (:parents @request) (:id (:request @dragging-request))))))
+                                                  (swap! request-json assoc (:uuid @request) @request)
+                                                  (swap! connector-positions assoc id (position (elem-by-id id)))
+                                                  (swap! lines assoc (str (:id (:request @dragging-request)) "-" (:id @request))
+                                                         {:start-pos (:connector-id @dragging-request)
+                                                          :end-pos   id})
+                                                  (reset! dragging-request nil)))
+                              :cx             (:x pos)
+                              :cy             (:y pos)
+                              :r              6
+                              :stroke         @border-color
+                              :visibility     (if visible?
+                                                "visible"
+                                                "hidden")
+                              :fill           "white"}]))
 
 (defn request-node []
   (reagent/with-let [uuid (.toString (random-uuid))
+                     connector-ids {:top    (str uuid "-top")
+                                    :bottom (str uuid "-bottom")
+                                    :left   (str uuid "-left")
+                                    :right  (str uuid "-right")}
                      request (reagent/atom {:uuid uuid :serviceName "some service" :parents (set '())})
                      selected? (reagent/atom false)
                      mouse-over? (reagent/atom false)
                      border-color (reagent/atom "black")
                      z-index (reagent/atom '())
                      start-pos (reagent/atom {:x 100 :y 150})
-                     current-pos (reagent/atom {:x 100 :y 150})
-                     ignored (swap! node-positions assoc uuid @current-pos)]
-                    [:div {:id             uuid
-                           :on-mouse-over  #(reset! mouse-over? true)
-                           :on-mouse-leave #(reset! mouse-over? false)
-                           :style          (into {:width            100
-                                                  :height           64
-                                                  :left             (:x @current-pos)
-                                                  :top              (:y @current-pos)
-                                                  :text-align       "center"
-                                                  :vertical-align   "middle"
-                                                  :background-color "white"
-                                                  :position         "absolute"}
-                                                 (when (not (empty? @z-index))
-                                                   [{:z-index (first @z-index)}]))}
-                     [:div {:on-mouse-over  #(reset! border-color "red")
-                            :on-mouse-up    (fn [_]
-                                              (reset! selected? false)
-                                              (reset! current-request request)
-                                              (swap! z-index #(drop 1 %)))
-                            :on-mouse-leave (fn [_]
-                                              (reset! selected? false)
-                                              (swap! z-index #(drop 1 %))
-                                              (reset! border-color "black"))
-                            :on-mouse-down  (fn [evt]
-                                              (reset! selected? true)
-                                              (when (empty? @z-index)
-                                                (swap! z-index conj (.-zIndex (.-target evt))))
-                                              (swap! z-index conj 2147483647)
-                                              (swap! start-pos assoc :x (.-clientX evt) :y (.-clientY evt)))
-                            :on-mouse-move  (fn [evt]
-                                              (when @selected?
-                                                (swap! current-pos assoc
-                                                       :x (- (+ (:x @current-pos) (.-clientX evt)) (:x @start-pos))
-                                                       :y (- (+ (:y @current-pos) (.-clientY evt)) (:y @start-pos)))
-                                                (swap! start-pos assoc :x (.-clientX evt) :y (.-clientY evt))
-                                                (swap! node-positions assoc uuid {:x (.-offsetLeft (.-parentElement (.-target evt)))
-                                                                                  :y (.-offsetTop (.-parentElement (.-target evt)))})))
-                            :style          {:width            100
-                                             :height           64
-                                             :text-align       "center"
-                                             :vertical-align   "middle"
-                                             :border-style     "solid"
-                                             :border-color     @border-color
-                                             :background-color "white"}}
+                     current-pos (reagent/atom {:x 100 :y 150})]
+                    [:g {:id             uuid
+                         :transform      (str "translate(" (:x @current-pos) "," (:y @current-pos) ")")
+                         :on-mouse-over  (fn [_]
+                                           (reset! mouse-over? true))
+                         :on-mouse-leave (fn [_]
+                                            (reset! mouse-over? false))}
+                     [:rect {:id             uuid
+                             :on-mouse-over  (fn [_]
+                                               (reset! border-color "red"))
+                             :on-mouse-up    (fn [_]
+                                               (reset! selected? false)
+                                               (reset! current-request request)
+                                               (swap! z-index #(drop 1 %)))
+                             :on-mouse-leave (fn [_]
+                                               (reset! selected? false)
+                                               (swap! z-index #(drop 1 %))
+                                               (reset! border-color "black"))
+                             :on-mouse-down  (fn [evt]
+                                               (reset! selected? true)
+                                               (when (empty? @z-index)
+                                                 (swap! z-index conj (.-zIndex (.-target evt))))
+                                               (swap! z-index conj 2147483647)
+                                               (swap! start-pos assoc :x (.-clientX evt) :y (.-clientY evt)))
+                             :on-mouse-move  (fn [evt]
+                                               (when @selected?
+                                                 (swap! current-pos assoc
+                                                        :x (- (+ (:x @current-pos) (.-clientX evt)) (:x @start-pos))
+                                                        :y (- (+ (:y @current-pos) (.-clientY evt)) (:y @start-pos)))
+                                                 (swap! start-pos assoc :x (.-clientX evt) :y (.-clientY evt))
+                                                 (doseq [id (keys @connector-positions)]
+                                                   (swap! connector-positions assoc id (position (elem-by-id id))))))
+                             :width          100
+                             :height         64
+                             :stroke         @border-color
+                             :fill           "white"}]
+                     [:text
+                      {:x (/ (- 100 (* 7 (count (:serviceName @request)))) 2)
+                       :y 32}
                       (:serviceName @request)]
-                     [connector request {:x 43 :y -7} @mouse-over?]
-                     [connector request {:x 43 :y 59} @mouse-over?]
-                     [connector request {:x -10 :y 26} @mouse-over?]
-                     [connector request {:x 95 :y 26} @mouse-over?]]
+                     [connector (:top connector-ids) request {:x 50 :y 0} @mouse-over?]
+                     [connector (:bottom connector-ids) request {:x 50 :y 64} @mouse-over?]
+                     [connector (:left connector-ids) request {:x 0 :y 32} @mouse-over?]
+                     [connector (:right connector-ids) request {:x 100 :y 32} @mouse-over?]]
                     ))
 
 (defn draw-lines []
@@ -204,17 +177,29 @@
     (for [l (vals @lines)]
       [line (:start-pos l) (:end-pos l)])))
 
+(defn arrow-head []
+  [:defs
+   [:marker {:id           "Triangle"
+             :viewBox      "0 0 10 10"
+             :refX         "10"
+             :refY         "5"
+             :markerWidth  "6"
+             :markerHeight "6"
+             :orient       "auto"}
+    [:path {:d "M 0 0 L 10 5 L 0 10 z"}]]])
+
 (defn graph-ui []
-  (into (into
-          [:div {:id "request_graph"}
-           [:h2 "Graph"]
-           [:div {:style {:width            640
-                          :height           200
-                          :background-color "gray"}}
-            [request-icon]]
-           [request-detail]]
-          (repeat @request-count [request-node]))
-        (draw-lines)))
+  [:div {:id "request_graph"}
+   [:h2 "Graph"]
+   (-> [:svg {:id     "request-graph-svg"
+              :width  640
+              :height 400}
+        [arrow-head]
+        [request-icon]]
+       (into (repeat @request-count [request-node]))
+       (into (draw-lines)))
+   [request-detail]]
+  )
 
 
 (defn interface []
